17e075bb5665e1cc0b925cd56445414275e881db
Carlos Gabriel <82345279+eucgabriel@users.noreply.github.com>
2022-10-25 11:45:00 -0300

    ex046

---
+++ Aula 13.py
@@ -0,0 +1,16 @@
+##Teste 1
+for c in range(0, 6, 2):
+    print(c)
+print('FIM')
+##Teste 2
+n = int(input('Digite um nº:'))
+for c in range(0, n+1):
+    print(c)
+print('FIM')
+##Teste 3
+i = int(input('Início:'))
+f = int(input('Fim:'))
+p = int(input('Passo:'))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')
---
+++ Exercícios/ex046.py
@@ -0,0 +1,5 @@
+import time
+f= 'Fogos!!!!'
+for c in range(10, 0, -1):
+    print(f, c)
+    time.sleep(1)
