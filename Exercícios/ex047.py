25444c7a97817761bbe4c2ad721dbf37b30c346e
Carlos Gabriel <82345279+eucgabriel@users.noreply.github.com>
2023-07-29 23:53:13 -0300

    ex048

--- Exercícios/ex047.py
+++ Exercícios/ex047.py
@@ -1,3 +1,3 @@
 print('Esses são os nºs pares de 0 a 50:')
-for c in range(2, 52, 2):
+for c in range(2, 51, 2):
     print(c)
---
+++ Exercícios/ex048.py
@@ -0,0 +1,4 @@
+print('Esses são os nºs ímpares e múltiplos de 3')
+for c in range(0,500):
+    if (c % 3 == 0) and (c % 2 != 0):
+        print(c)
